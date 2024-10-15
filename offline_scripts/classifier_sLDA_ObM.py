9d63a7cb7ba1325df1accba0dd7361e27f8af8cd
Markus Oberndorfer <markusoberndorfer@M2AiRKUS.fritz.box>
2024-10-15 17:22:47 +0200

    adaptation of dummy file

--- offline_scripts/classifier_sLDA_ObM.py
+++ offline_scripts/classifier_sLDA_ObM.py
@@ -4,83 +4,49 @@ Authors: Mirage 91
 """
 ''' IMPORTS PYTHON'''
 import numpy as np
-import matplotlib.pyplot as plt
+from numpy import load
+import mne
+from mne.decoding import CSP
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
+from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
+import pickle
 
-''' IMPORTS CUSTOM '''
-from custom_functions import Pilot
-from classifier_functions import sLDA
-from classifier_functions import AdvancedPipelineEvaluator
 
 ''' SETTINGS '''
-pilot_name = 'Antonio'
+...
 
-# ALL Sessions 4 classes 18th + 26th July 2024
-# path = [
-#         r'C:\Users\Markus\Mirage91\Program\venv\mirage91\recordings\2024_07_18\block_1.xdf',
-#         r'C:\Users\Markus\Mirage91\Program\venv\mirage91\recordings\2024_07_18\block_2.xdf',
-#         r'C:\Users\Markus\Mirage91\Program\venv\mirage91\recordings\2024_07_18\block_3.xdf',
-#         r'C:\Users\Markus\Mirage91\Program\venv\mirage91\recordings\2024_07_26\block_1.xdf',
-#         r'C:\Users\Markus\Mirage91\Program\venv\mirage91\recordings\2024_07_26\block_2.xdf',
-#         r'C:\Users\Markus\Mirage91\Program\venv\mirage91\recordings\2024_07_26\block_3.xdf'
-#         ]
-# #Session 26th July 2024
-# path = [
-#         r'C:\Users\Markus\Mirage91\Program\venv\mirage91\recordings\2024_07_26\block_1.xdf',
-#         r'C:\Users\Markus\Mirage91\Program\venv\mirage91\recordings\2024_07_26\block_2.xdf',
-#         r'C:\Users\Markus\Mirage91\Program\venv\mirage91\recordings\2024_07_26\block_3.xdf'
-#         ]
+''' MAIN '''    
 
-#Session 18th July 2024
-path = [
-        r'C:\Users\Markus\Mirage91\Program\venv\mirage91\recordings\2024_07_18\block_1.xdf',
-        r'C:\Users\Markus\Mirage91\Program\venv\mirage91\recordings\2024_07_18\block_2.xdf',
-        r'C:\Users\Markus\Mirage91\Program\venv\mirage91\recordings\2024_07_18\block_3.xdf'
-        ]
-paradigm = {'pre_cue':2.0, 'left_hand':4.5, 'left_foot':4.5, 'right_foot':4.5, 'mental_singing':4.5, 'pause':2.5, 'post_run':0.0}
-tasks = ['left_hand', 'left_foot', 'right_foot', 'mental_singing']
+data = load('./features/csp_features.npy', allow_pickle=True)
+features = np.vstack(data[:, 1])
+labels = [str(elem) for elem in data[:, 0]]
+
+# Calculate CSP features
+n_classes = 4 # Assuming 4 classes
+n_components = 10  # Number of components for CSP
+csp = CSP(n_components=n_components, log=True, cov_est='epoch')
+
+# Define sLDA
+slda = LDA(shrinkage='auto', solver='eigen')
+name = 'sLDA'
+
+cv = StratifiedKFold(n_splits=10)
+
+# Calcualte cross-val
+cross_val_scores = cross_val_score(estimator=slda, X=features, y=labels, cv=cv, scoring='accuracy')
+print(f'{name} 10-fold cross-validation accuracy: {cross_val_scores.mean():.4f} ± {cross_val_scores.std():.4f}')
+
+# Split data into training and testing
+# X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+# Train sLDA classifier
+
+# slda.fit(X_train, y_train)
+
+# Make predictions
+# y_predict = slda.predict(X=X_test)
 
 
-# Session 10th June 2024
-# path = [r'C:\Users\Markus\Mirage91\Program\mirage91\recordings\2024_06_10\block_1.xdf']
-# paradigm = {'pre_cue':2.0, 'hand':4.5, 'foot':4.5, 'pause':2.5}
-# tasks = ['hand', 'foot']
-
-''' MAIN '''
-# define pilot
-pilot = Pilot(pilot_name)
-
-# load eeg and process it
-pilot.set_eeg(path, paradigm, tasks)
-pilot.eeg.processing_pipeline()
-#pilot.eeg.show_erds()
-# pilot.eeg.show_erds_bp(tasks)
-
-# get train and test set
-X_train, X_test, Y_train, Y_test = pilot.eeg.split_train_test()
-
-# extract features bandpower
-print('\n BANDPOWER + LDA: \n ')
-modality = 'bp'
-pilot.eeg.extract_features(modality=modality)
-X_train_feat, X_test_feat, Y_train_feat, Y_test_feat = pilot.eeg.get_features()
-# choose classifier
-classifier = sLDA(X_train_feat, X_test_feat, Y_train_feat, Y_test_feat)
-classifier.train_and_test(modality=modality)
-
-# extract features csp
-print('\n CSP + LDA: \n')
-modality = 'csp'
-pilot.eeg.extract_features(modality=modality)
-X_train_feat, X_test_feat, Y_train_feat, Y_test_feat = pilot.eeg.get_features()
-# choose classifier
-classifier = sLDA(X_train_feat, X_test_feat, Y_train_feat, Y_test_feat)
-classifier.train_and_test(modality=modality)
-
-# print('\n PIPELINE TESTING ... \n')
-# ## ONLY FOR EVALUATING NEW THINGS
-# # find best pipeline and parameters automatically (see SLDA.PY)
-# evaluator = AdvancedPipelineEvaluator(X_train, X_test, Y_train, Y_test)
-# evaluator.evaluate_all_pipelines()
-
-# show results
-plt.show()
+# Save the CSP and sLDA model weights
+with open('csp_slda_weights.pkl', 'wb') as f:
+    pickle.dump({'csp': csp, 'slda': slda}, f)
